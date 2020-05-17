8c0b2df81f52f7eadc3e7cee672466315d7c0d16
Dun <kathymaidung@gmail.com>
2020-05-17 20:55:12 +0300

    Grunt Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,18 +6,20 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     //Automatically load required Grunt tasks
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     //Define the configuration for all the tasks
     grunt.initConfig({
-        sass:{
-            dist:{
-                files:{
+        sass: {
+            dist: {
+                files: {
                     'css/styles.css': 'css/styles.scss'
                 }
             }
         },
-        watch:{
+        watch: {
             files: 'css/*.scss',
             tasks: ['sass']
         },
@@ -37,10 +39,127 @@ module.exports = function (grunt) {
                     }
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif}'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name:'cssmin',
+                            createConfig: function (context, block) {
+                                var generated = context.options.generated;
+                                generated.options={
+                                    keepSpectialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            options: {
+                assetDirs: ['dist', 'dist.css', 'dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace:true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html':'dist/aboutus.html'
+                }
+            }
         }
     });
 
-    grunt.registerTask('css',['sass']);
+    grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
-
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 };
